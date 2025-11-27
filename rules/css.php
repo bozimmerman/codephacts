2ff319694cc5aa0e8562f57fb2da7601499af7c7
bozimmerman <bo@zimmers.net>
2025-11-27 00:17:45 +0000

    git-svn-id: svn://192.168.1.10/public/CodePhacts@24875 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ rules/css.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ Copyright 2025-2025 Bo Zimmerman
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+return [
+    'extensions' => ['css', 'scss', 'sass', 'less'],
+    'language' => 'CSS',
+    'analyzer' => function(&$stats, $lines)
+    {
+        $inBlockComment = false;
+        foreach ($lines as $line)
+        {
+            $trimmed = trim($line);
+            
+            if (empty($trimmed))
+            {
+                $stats['blank_lines']++;
+                continue;
+            }
+            if (strpos($trimmed, '/*') !== false && strpos($trimmed, '*/') === false)
+            {
+                $inBlockComment = true;
+                $stats['comment_lines']++;
+                $stats['ncloc']++;
+                continue;
+            }
+            if ($inBlockComment)
+            {
+                $stats['comment_lines']++;
+                $stats['ncloc']++;
+                if (strpos($trimmed, '*/') !== false)
+                    $inBlockComment = false;
+                continue;
+            }
+            if (strpos($trimmed, '/*') !== false && strpos($trimmed, '*/') !== false)
+            {
+                $stats['comment_lines']++;
+                $stats['ncloc']++;
+                continue;
+            }
+            $stats['ncloc']++;
+            $declarations = substr_count($line, ';');
+            $stats['code_statements'] += $declarations;
+        }
+        return $stats;
+    }
+];
