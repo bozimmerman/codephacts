3a4dd767616b3e9f8cdfa18c558383792e8d41b1
bozimmerman <bo@zimmers.net>
2025-12-03 06:12:11 +0000

    git-svn-id: svn://192.168.1.10/public/CodePhacts@24959 0d6f1817-ed0e-0410-87c9-987e46238f29

--- public/estimation_functions.php
+++ public/estimation_functions.php
@@ -28,6 +28,15 @@
  */
 function calculateCOCOMO($kloc, $mode = 'semi-detached')
 {
+    if ($kloc <= 0) {
+        return [
+            'effort' => 0,
+            'time' => 0,
+            'people' => 0,
+            'mode' => $mode
+        ];
+    }
+    
     $coefficients = [
         'organic' => ['a' => 2.4, 'b' => 1.05, 'c' => 2.5, 'd' => 0.38],
         'semi-detached' => ['a' => 3.0, 'b' => 1.12, 'c' => 2.5, 'd' => 0.35],
@@ -40,7 +49,7 @@ function calculateCOCOMO($kloc, $mode = 'semi-detached')
     $coef = $coefficients[$mode];
     $effort = $coef['a'] * pow($kloc, $coef['b']);
     $time = $coef['c'] * pow($effort, $coef['d']);
-    $people = $effort / $time;
+    $people = $time > 0 ? $effort / $time : 0;
     
     return [
         'effort' => $effort,
@@ -59,6 +68,16 @@ function calculateCOCOMO($kloc, $mode = 'semi-detached')
  */
 function calculateCOCOMO2($kloc, $scaleFactors = null, $effortMultipliers = null)
 {
+    // Protect against zero or negative input
+    if ($kloc <= 0) {
+        return [
+            'effort' => 0,
+            'time' => 0,
+            'people' => 0,
+            'exponent' => 0.91
+        ];
+    }
+    
     if ($scaleFactors === null) {
         $scaleFactors = [
             'PREC' => 3.0,  // Precedentedness
@@ -71,15 +90,15 @@ function calculateCOCOMO2($kloc, $scaleFactors = null, $effortMultipliers = null
     $B = 0.91 + 0.01 * array_sum($scaleFactors);
     $A = 2.94;
     $effort = $A * pow($kloc, $B);
-    if ($effortMultipliers !== null) 
+    if ($effortMultipliers !== null)
     {
-        foreach ($effortMultipliers as $em) 
+        foreach ($effortMultipliers as $em)
             $effort *= $em;
     }
     $C = 3.67;
     $D = 0.28 + 0.2 * ($B - 0.91);
     $time = $C * pow($effort, $D);
-    $people = $effort / $time;
+    $people = $time > 0 ? $effort / $time : 0;
     return [
         'effort' => $effort,
         'time' => $time,
@@ -97,6 +116,18 @@ function calculateCOCOMO2($kloc, $scaleFactors = null, $effortMultipliers = null
  */
 function calculateFunctionPoints($loc, $language = 'PHP')
 {
+    if ($loc <= 0) 
+    {
+        return [
+            'functionPoints' => 0,
+            'effort' => 0,
+            'time' => 0,
+            'people' => 0,
+            'language' => $language,
+            'locPerFP' => 60
+        ];
+    }
+    
     $locPerFP = [
         'Assembly' => 320,
         'C' => 128,
@@ -118,7 +149,7 @@ function calculateFunctionPoints($loc, $language = 'PHP')
     $hoursPerFP = 7.5; // median
     $effort = ($functionPoints * $hoursPerFP) / 160; // Convert to person-months
     $time = 2.5 * pow($effort, 0.38);
-    $people = $effort / $time;
+    $people = $time > 0 ? $effort / $time : 0;
     return [
         'functionPoints' => $functionPoints,
         'effort' => $effort,
@@ -137,13 +168,22 @@ function calculateFunctionPoints($loc, $language = 'PHP')
  */
 function calculateSLIM($loc, $productivity = 5000)
 {
+    if ($loc <= 0 || $productivity <= 0) {
+        return [
+            'effort' => 0,
+            'time' => 0,
+            'people' => 0,
+            'productivity' => $productivity
+        ];
+    }
+    
     $kloc = $loc / 1000;
     $effort = 3.0 * pow($kloc, 1.12);
-    for ($i = 0; $i < 10; $i++) 
+    for ($i = 0; $i < 10; $i++)
     {
         $time = pow($loc / ($productivity * pow($effort, 1/3)), 0.75);
         $newEffort = pow($loc / ($productivity * pow($time, 4/3)), 3);
-        if (abs($newEffort - $effort) < 0.01) 
+        if (abs($newEffort - $effort) < 0.01)
         {
             $effort = $newEffort;
             break;
@@ -151,7 +191,7 @@ function calculateSLIM($loc, $productivity = 5000)
         $effort = ($effort + $newEffort) / 2;
     }
     $time = pow($loc / ($productivity * pow($effort, 1/3)), 0.75);
-    $people = $effort / $time;
+    $people = $time > 0 ? $effort / $time : 0;
     return [
         'effort' => $effort,
         'time' => $time,
@@ -168,6 +208,16 @@ function calculateSLIM($loc, $productivity = 5000)
  */
 function calculatePutnam($loc, $technology = 8000)
 {
+    if ($loc <= 0 || $technology <= 0) {
+        return [
+            'effort' => 0,
+            'time' => 0,
+            'people' => 0,
+            'technology' => $technology,
+            'minTime' => 0
+        ];
+    }
+    
     $kloc = $loc / 1000;
     $k1 = 0.15;
     $k2 = 0.4;
@@ -176,7 +226,7 @@ function calculatePutnam($loc, $technology = 8000)
     $effort = pow($kloc / ($technology / 1000 * pow($time, 4/3)), 3);
     $timeMonths = $time * 12;
     $effortMonths = $effort * 12;
-    $people = $effortMonths / $timeMonths;
+    $people = $timeMonths > 0 ? $effortMonths / $timeMonths : 0;
     return [
         'effort' => $effortMonths,
         'time' => $timeMonths,
@@ -195,6 +245,16 @@ function calculatePutnam($loc, $technology = 8000)
  */
 function generateEstimates($loc, $primaryLanguage = 'PHP', $cocomoMode = 'semi-detached')
 {
+    if ($loc <= 0) {
+        return [
+            'cocomo' => calculateCOCOMO(0, $cocomoMode),
+            'cocomo2' => calculateCOCOMO2(0),
+            'functionPoints' => calculateFunctionPoints(0, $primaryLanguage),
+            'slim' => calculateSLIM(0),
+            'putnam' => calculatePutnam(0)
+        ];
+    }
+    
     $kloc = $loc / 1000;
     
     return [
